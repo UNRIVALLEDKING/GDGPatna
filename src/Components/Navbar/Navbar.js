0ac8051b8ce9a376e2bbdb716100458be2d296c6
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-05-11 01:51:09 +0530

    nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,13 @@
 import React from "react";
+import "./Navbar.css";
+import {
+  AiOutlineTeam,
+  AiOutlineHome,
+  AiOutlineComment,
+  AiOutlineForm,
+} from "react-icons/ai";
+import { MdOutlineEvent } from "react-icons/md";
+import { BsQuestionCircle } from "react-icons/bs";
 
 export default function Navbar() {
   return (
@@ -7,14 +16,14 @@ export default function Navbar() {
         <div className="container">
           <a className="navbar-brand" href="#">
             <img
-              style={{ width: "50px" }}
+              style={{ width: "50px", marginRight: "5px" }}
               src="https://i.ibb.co/Hp0SX43/gdg.png"
               alt="gdg"
               border="0"
             />
             <h1 className="h5 d-inline">GDG Patna</h1>
           </a>
-          <button
+          <span
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
@@ -23,72 +32,158 @@ export default function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Gallery
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Feeds
-                  </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Join Us
-                  </a>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        As a Member
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        As a Volunteer
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        As a Leader
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link">Contact Us</a>
-                </li>
-              </ul>
-            </div>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </span>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  <AiOutlineHome style={{ color: "#ea4335" }} size={25} />
+                  <span>Home</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <AiOutlineTeam style={{ color: "#4285f4" }} size={25} />
+                  <span>Team</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <MdOutlineEvent style={{ color: "#0f9d58" }} size={25} />{" "}
+                  <span>Events</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">
+                  <AiOutlineComment style={{ color: "#fbbc04" }} size={25} />
+                  <span>About</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">
+                  <BsQuestionCircle style={{ color: "#ea4335" }} size={25} />
+                  <span>FAQ</span>
+                </a>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <AiOutlineForm style={{ color: "#4285f4" }} size={25} /> Join
+                  Us
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a
+                      style={{ color: "#0f9d58" }}
+                      className="dropdown-item"
+                      href="#"
+                    >
+                      As a Member
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      style={{ color: "#fbbc04" }}
+                      className="dropdown-item"
+                      href="#"
+                    >
+                      As a Volunteer
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      style={{ color: "#ea4335" }}
+                      className="dropdown-item"
+                      href="#"
+                    >
+                      As a Leader
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
+
+      {/* navbar 2 */}
+
+      {/* <nav class="navbar navbar-expand-custom navbar-mainbg">
+        <a className="navbar-brand" href="#">
+          <img
+            style={{ width: "50px" }}
+            src="https://i.ibb.co/Hp0SX43/gdg.png"
+            alt="gdg"
+            border="0"
+          />
+          <h1 className="h5 d-inline">GDG Patna</h1>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <GoThreeBars />
+        </button>
+        <div>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <div class="hori-selector">
+                <div class="left"></div>
+                <div class="right"></div>
+              </div>
+              <li class="nav-item active">
+                <a class="nav-link" href="javascript:void(0);">
+                  <AiOutlineHome /> Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0);">
+                  <AiOutlineTeam />
+                  Team
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0);">
+                  <MdOutlineEvent />
+                  Events
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0);">
+                  <AiOutlineComment />
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0);">
+                  <BsQuestionCircle />
+                  FAQ
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0);">
+                  <AiOutlineForm />
+                  Join Us
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav> */}
     </>
   );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,94 @@
+import { Button } from "@mui/material";
 import React from "react";
+import {
+  AiFillTwitterCircle,
+  AiFillYoutube,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { BsTelegram, BsFacebook } from "react-icons/bs";
+import "./Home.css";
 
 export default function Home() {
   return (
     <>
-      <div>Home</div>
+      <div className="container">
+        <div
+          style={{ backgroundColor: "#1a73e8" }}
+          className="text-white p-4 p-md-5 my-4 rounded"
+        >
+          <div className="row">
+            <div className="col-md-6 px-0">
+              <h1 className="display-6 fst-italic">
+                The Community of Developers, For Developers, By Developers
+              </h1>
+              <p className="lead my-3">
+                Our ultimate goal is to bring together all developers of Patna
+                to create a thriving developer ecosystem.
+              </p>
+
+              <ul className="sns-link-home">
+                <li>
+                  <a target="_blank" href="https://twitter.com/gdgpatna">
+                    <AiFillTwitterCircle size={25} />
+                  </a>
+                </li>
+                <li>
+                  <a target="_blank" href="https://www.facebook.com/gdgpatna">
+                    <BsFacebook size={25} />
+                  </a>
+                </li>
+                <li>
+                  <a target="_blank" href="https://t.me/gdgpatna">
+                    <BsTelegram size={25} />
+                  </a>
+                </li>
+                <li>
+                  <a target="_blank" href="https://www.youtube.com/c/GDGPatna">
+                    <AiFillYoutube size={25} />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    href="https://www.linkedin.com/company/gdg-patna"
+                  >
+                    <AiFillLinkedin size={25} />
+                  </a>
+                </li>
+              </ul>
+              <Button
+                style={{
+                  color: "white",
+                  border: "1px solid white",
+                  marginRight: "5px",
+                }}
+                variant="outlined"
+              >
+                Learn More
+              </Button>
+              <Button
+                className="d-inline-block d-md-none"
+                style={{ color: "white", border: "1px solid white" }}
+                variant="outlined"
+              >
+                Join Us
+              </Button>
+            </div>
+            <div className="col-md-6 px-2">
+              <iframe
+                className="rounded mt-3 mt-md-0"
+                width="100%"
+                height="270"
+                src="https://www.youtube.com/embed/V6s5kjSoqzw"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
